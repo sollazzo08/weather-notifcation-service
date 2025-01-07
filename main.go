1765e465b199d6d0eb71b8cca47ccf7eff2fe5c2
Sollazzo08 <msollazzo08@gmail.com>
2025-01-06 22:56:30 -0500

    initialized sevrver

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+    "net/http"
 )
 
 func main() {
-    fmt.Println("Weather Project, to be started...")
+    fmt.Println("Starting Project...")
+    // ListenAndSever fucntion starts the server and binds it to 8080. WHenever it receives an http request it will hand the request off the http.Handler function. In our case its http.FileServer
+    http.ListenAndServe(":8080",http.FileServer(http.Dir(".")))
 }
